8e33a104d66a2df0a1fbdd6a4608de9b448cc09c
OanaBogdan <101863311+OanaBogdan@users.noreply.github.com>
2023-11-20 20:01:04 +0200

    create project

---
+++ lab_1_sp/src/main/java/com/example/lab_1_sp/Lab1SpApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab_1_sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1SpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1SpApplication.class, args);
+    }
+
+}
